4211e8a73f0cbd0e51f65b054bde788324f43761
Yamila-Vecces <yamilavecces@gmail.com>
2021-09-26 22:59:24 -0300

    Subo tp crud

---
+++ src/controllers/productsController.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const finalPrice = (price, discount) => Math.round(price - (price * (discount/100)))
+
+const controller = {
+	// Root - Show all products
+	index: (req, res) => {
+		res.render('products', {
+			products,
+			toThousand
+		})
+	},
+
+	// Detail - Detail from one product
+	detail: (req, res) => {
+		const {id} = req.params
+		const productDetail = products.find(e => e.id === +id)
+		res.render('detail', {productDetail, toThousand, finalPrice})
+	},
+
+	// Create - Form to create
+	create: (req, res) => {
+		res.render('product-create-form')
+	},
+	
+	// Create -  Method to store
+	store: (req, res) => {
+        const object = req.body
+		object.id = products.length
+		products.push(product)
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2))
+		res.redirect('/products/detail/${product.id}')
+	},
+
+	// Update - Form to edit
+	edit: (req, res) => {
+		products.find(e => e.id === req.params.id)
+		res.render('product-edit-form', {products})
+	},
+	// Update - Method to update
+	update: (req, res) => {
+		const productUpdate = products.Find(product => product.id === +req.params.id)
+		const {name, price, discount, category, description} = req.body
+		if (productUpdate) {
+            productUpdate.name = name
+            productUpdate.price = +price
+			productUpdate.discount = +discount
+            productUpdate.category = category
+            productUpdate.description = description
+
+			fs.writeFileSync(productsFilePath, JSON.stringify(products))
+
+            res.redirect('/products/detail/+req.params.id')
+		} else {
+			res.redirect('/')
+		}
+	},
+
+	// Delete - Delete one product from DB
+	destroy : (req, res) => {
+		products = products.filter(product => product.id === +req.params.id)
+
+		fs.writeFileSync(productsFilePath, JSON.stringify(products))
+
+        res.redirect('/')
+	}
+};
+
+module.exports = controller;
